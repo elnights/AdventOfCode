10803f282217ee3508398031305ba52d648aeb1d
elnights <elnights@gmail.com>
2023-12-09 03:09:20 +0200

    2015 Day 5/1

---
+++ 2015/5/1/index.js
@@ -0,0 +1,16 @@
+const fs = require('node:fs');
+const input = fs.readFileSync('../input', 'utf-8').trimEnd();
+const lines = input.split('\n');
+
+const vowels = /[aeiou]/g;
+const forbidden = /ab|cd|pq|xy/;
+const twoInARow = /(\w)\1/;
+
+let niceStrings = lines.filter(line => {
+    let vowelsMatch
+    if ((line.match(vowels) ?? []).length < 3) return false;
+    if (line.match(forbidden)) return false;
+    return line.match(twoInARow);
+});
+
+console.log(niceStrings.length);
